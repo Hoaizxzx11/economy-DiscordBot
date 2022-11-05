4e139f3a9c9711ccc2d232dbdb4bbffe18034659
Hoaizxzx11 <112836123+Hoaizxzx11@users.noreply.github.com>
2022-11-05 18:25:57 +0700

    Update and rename economy/mining.js to nền kinh tế/mining.js

--- nền kinh tế/mining.js
+++ nền kinh tế/mining.js
@@ -9,6 +9,6 @@ $color[$random[10000;999999]]
 $description[Bạn giải được bài toán và được cô giáo cho $random[0;5] hộp quà!, \`$getServerVar[prefix]hopqua\` để đổi lấy 5 điểm GPA.]
 $globalCooldown[2s;**⏰ Chờ %time% cô giáo đi livetream Tiktok và sẽ quay trở lại ngay.!**]
 
-$onlyIf[$getGlobalUserVar[health]>=30; Trí thông minh của bạn ở mức 30% và không thể học tiếp! Dùng điểm GPA để đổi lấy thuốc bổ não.]`
-$onlyIf[$getGlobalUserVar[toan]>=1; Bạn không có sách để học!] `
+$ onlyIf [$ getGlobalUserVar [sức khỏe]> = 30; Trí thông minh của bạn ở mức 30% và không thể học tiếp! Use GPA point to change get plug-in.] `
 }
+
