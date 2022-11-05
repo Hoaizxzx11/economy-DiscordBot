b1a34cb62a73526428c2d63e0cb305b440000e6e
Hoaizxzx11 <112836123+Hoaizxzx11@users.noreply.github.com>
2022-11-05 19:41:52 +0700

    Update and rename fish.js to tienganh.js

--- economy/tienganh.js
+++ economy/tienganh.js
@@ -1,9 +1,9 @@
 module.exports = {
   
   name: "tienganh",
-  code: `$description[Học tiếng anh và nói được từ $randomText[Hello;Hi;What;I love you cô Thảo;Where's are you from; Welcome to RoomRoom;],Bạn được cô giáo Thảo cho **$random[0;5] hộp quà.** \`$getServerVar[prefix]hopqua\` để mở hộp quà lấy 5 điểm GPA]
+  code: `$description[Học tiếng anh và nói được từ $randomText[Hello;Hi;What;I love you cô Thảo;Where's are you from; Welcome to RoomRoom;],Bạn được cô giáo Thảo cho **$random[0;1] hộp quà.** \`$getServerVar[prefix]hopqua\` để mở hộp quà lấy 5 điểm.]
 $color[$random[0;999999]]
-$setGlobalUserVar[fish;$sum[$getGlobalUserVar[fish];$random[0;5]]]
+$setGlobalUserVar[tienganh;$sum[$getGlobalUserVar[tienganh];$random[0;1]]]
 $setGlobalUserVar[health;$sub[$getGlobalUserVar[health];$random[0;5]]]
 
 $globalCooldown[2m;**⏰ Chờ %time% Cô giáo Thảo đi dự giờ và sẽ quay trở lại!**]
